f84b29b393b3caa64cdea053be97d7a76e7b2a85
Daniel <danielz.nas@gmail.com>
2025-01-28 12:07:01 -0400

    trouble shooted html not connecting properly to the js file

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-alert("js file works!");
+const buttonTest = document.querySelector('#test');
+
+buttonTest.addEventListener('click', function(){
+    test();
+});
+
+let test = function () {
+    console.log('js file linked to html');
+    alert('js is connected to the html');
+}; 
