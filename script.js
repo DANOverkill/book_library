41187453307a34043d9c89cd373ff9d6d79d28f7
Daniel <danielz.nas@gmail.com>
2025-01-30 09:37:25 -0400

    added a the book constructor and a test book01 oject constant

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ let test = function () {
     console.log('js file linked to html');
     alert('js is connected to the html');
 }; 
+
+
+function book(name, genre, author, pages, read) {
+    this.name = name;
+    this.genre = genre;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.haveRead = function() {
+        console.log(`I have ${this.read} ${this.name}`);
+    };
+};
+
+const book01 = new book('the Lord of the Rings', 'Fantasy', 'J.J.R. Tolkien', '1000', 'read');
